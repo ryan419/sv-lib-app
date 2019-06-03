8242b092b9b16dbe9a6a8b106c413a05851839b4
Ryan Xiao <ryan.shaw.0419@google.com>
2019-06-04 06:08:24 +1000

    add book module and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'book',
+  },
+  {
+    path: 'book',
+    loadChildren: () =>
+      import('./book/book.module').then(mod => mod.BookModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
